eb384de3adb9cc355eb71b54d6f592df26082c0e
Chanwit21 <bom.suthi@gmail.com>
2021-10-16 18:28:00 +0700

    user

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,6 @@ const authController = require('../controller/auth');
 const { upload } = require('../middleware/multer');
 
 router.post('/login', authController.login);
-router.post(
-  '/register',
-  upload.single('profile_image'),
-  authController.register
-);
+router.post('/register', upload.single('profile_image'), authController.register);
 
 module.exports = router;
