593d9f7ed2363526b65003f7696f52ba6cb6b154
Chanwit21 <bom.suthi@gmail.com>
2021-10-21 21:12:41 +0700

    fix bug Home Page,date:21-10-2021

--- controller/productController.js
+++ controller/productController.js
@@ -1,81 +1,76 @@
-const { Op } = require("sequelize");
-const { Product, FavoriteProduct, ProductImage, CartItem, Cart } = require("../models")
+const { Op } = require('sequelize');
+const { Product, FavoriteProduct, ProductImage, CartItem, Cart } = require('../models');
 
 // get all data
 exports.getProductById = async (req, res, next) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
     const findName = await Product.findOne({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
     const product = await Product.findAll({
       where: {
-        name: findName.name
-      }
-    })
+        name: findName.name,
+      },
+    });
 
     const productImage = await ProductImage.findAll({
       where: {
-        '$Product.name$': findName.name
+        '$Product.name$': findName.name,
       },
       include: {
         model: Product,
         // attributes: ['name', 'id']
-      }
-    })
+      },
+    });
 
-
-    res.json({ product, productImage })
+    res.json({ product, productImage });
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-}
+};
 
 exports.checkFavorite = async (req, res, next) => {
   try {
-    const { productName } = req.body
-    let IsFavorite = false
+    const { productName } = req.body;
+    let IsFavorite = false;
     const product = await Product.findAll({
       where: {
-        name: productName
-      }
-    })
-    const productId = []
-    product.forEach(item => productId.push({ id: item.id }))
+        name: productName,
+      },
+    });
+    const productId = [];
+    product.forEach((item) => productId.push({ id: item.id }));
     const favortie = await FavoriteProduct.findAll({
       where: {
         userId: req.user.id,
-        [Op.or]: [...productId]
-
-      }
-    })
+        [Op.or]: [...productId],
+      },
+    });
 
     if (favortie.length) {
       IsFavorite = true;
     }
 
-    res.status(201).json({ IsFavorite })
+    res.status(201).json({ IsFavorite });
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-}
+};
 exports.createFavorite = async (req, res, next) => {
   try {
     const { productId } = req.body;
     const data = await FavoriteProduct.create({
       productId,
-      userId: req.user.id
-    })
-    res.status(201).json({ data })
+      userId: req.user.id,
+    });
+    res.status(201).json({ data });
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-}
+};
 
 exports.deleteFavorite = async (req, res, next) => {
   try {
@@ -83,40 +78,35 @@ exports.deleteFavorite = async (req, res, next) => {
     const data = await FavoriteProduct.destroy({
       where: {
         productId,
-        userId: req.user.id
-      }
-    })
-    res.status(204)
+        userId: req.user.id,
+      },
+    });
+    res.status(204);
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-}
+};
 
 exports.createCartItem = async (req, res, next) => {
   try {
     const { quality, productId } = req.body; //quantity
-    let userCart = await Cart.findOne({ where: { userId: req.user.id } })
-    console.log(`userCart`, userCart)
+    let userCart = await Cart.findOne({ where: { userId: req.user.id } });
+    console.log(`userCart`, userCart);
 
     if (!userCart) {
       userCart = await Cart.create({
-        userId: req.user.id
-      })
+        userId: req.user.id,
+      });
     }
-
 
     const cartItem = await CartItem.create({
       isSelectToPay: false,
       quality,
       cartId: userCart.id,
       productId,
-
-    })
-    res.status(201).json({ cartItem })
+    });
+    res.status(201).json({ cartItem });
+  } catch (err) {
+    next(err);
   }
-  catch (err) {
-    next(err)
-  }
-}
-
+};
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const productController = require('../controller/productController')
+const express = require('express');
+const productController = require('../controller/productController');
 const passport = require('passport');
 
 const productRouter = express.Router();
 
-productRouter.get('/:id', productController.getProductById)
-productRouter.post('/checkFavorite', passport.authenticate('jwtAll', { session: false }), productController.checkFavorite)
-productRouter.post('/favorite', passport.authenticate('jwtAll', { session: false }), productController.createFavorite)
-productRouter.post('/cart', passport.authenticate('jwtAll', { session: false }), productController.createCartItem)
-productRouter.delete('/favorite/:productId', passport.authenticate('jwtAll', { session: false }), productController.deleteFavorite)
+productRouter.get('/:id', productController.getProductById);
+productRouter.post(
+  '/checkFavorite',
+  passport.authenticate('jwtAll', { session: false }),
+  productController.checkFavorite
+);
+productRouter.post('/favorite', passport.authenticate('jwtAll', { session: false }), productController.createFavorite);
+productRouter.post('/cart', passport.authenticate('jwtAll', { session: false }), productController.createCartItem);
+productRouter.delete(
+  '/favorite/:productId',
+  passport.authenticate('jwtAll', { session: false }),
+  productController.deleteFavorite
+);
 
 module.exports = productRouter;
