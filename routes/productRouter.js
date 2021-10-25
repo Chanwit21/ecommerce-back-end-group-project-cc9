ee025be9e72949c52b114b4d528b517038fc2e4f
Chanwit21 <bom.suthi@gmail.com>
2021-10-26 00:24:10 +0700

    AllProduct 10%, date:26-10-2021

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,20 +5,51 @@ const { upload } = require('../middleware/multer');
 
 const productRouter = express.Router();
 
-productRouter.get('/:productName', productController.getProductById)
-productRouter.post('/checkFavorite', passport.authenticate('jwtAll', { session: false }), productController.checkFavorite)
-productRouter.post('/favorite', passport.authenticate('jwtAll', { session: false }), productController.createFavorite)
-productRouter.post('/cart', passport.authenticate('jwtAll', { session: false }), productController.createCartItem)
-productRouter.delete('/favorite/:productId', passport.authenticate('jwtAll', { session: false }), productController.deleteFavorite)
+productRouter.get('/:productName', productController.getProductById);
+productRouter.post(
+  '/checkFavorite',
+  passport.authenticate('jwtAll', { session: false }),
+  productController.checkFavorite
+);
+productRouter.post('/favorite', passport.authenticate('jwtAll', { session: false }), productController.createFavorite);
+productRouter.post('/cart', passport.authenticate('jwtAll', { session: false }), productController.createCartItem);
+productRouter.delete(
+  '/favorite/:productId',
+  passport.authenticate('jwtAll', { session: false }),
+  productController.deleteFavorite
+);
 
 //*product-Summary
-productRouter.get('/', passport.authenticate('jwtAdmin', { session: false }), productController.getProductAll)
-productRouter.delete('/:productId', passport.authenticate('jwtAdmin', { session: false }), productController.deleteProduct)
-productRouter.post('/', passport.authenticate('jwtAdmin', { session: false }), upload.array('imageUrl', 7), productController.createNewProduct)
-productRouter.get('/productImage/:productId', passport.authenticate('jwtAdmin', { session: false }), productController.getProductImageByProductId)
-productRouter.put('/:productId', passport.authenticate('jwtAdmin', { session: false }), upload.array('imageUrl', 7), productController.updateProduct)
-productRouter.get('/readyToShip/:productId', passport.authenticate('jwtAdmin', { session: false }), productController.readyToShip)
+productRouter.get('/', passport.authenticate('jwtAdmin', { session: false }), productController.getProductAll);
+productRouter.delete(
+  '/:productId',
+  passport.authenticate('jwtAdmin', { session: false }),
+  productController.deleteProduct
+);
+productRouter.post(
+  '/',
+  passport.authenticate('jwtAdmin', { session: false }),
+  upload.array('imageUrl', 7),
+  productController.createNewProduct
+);
+productRouter.get(
+  '/productImage/:productId',
+  passport.authenticate('jwtAdmin', { session: false }),
+  productController.getProductImageByProductId
+);
+productRouter.put(
+  '/:productId',
+  passport.authenticate('jwtAdmin', { session: false }),
+  upload.array('imageUrl', 7),
+  productController.updateProduct
+);
+productRouter.get(
+  '/readyToShip/:productId',
+  passport.authenticate('jwtAdmin', { session: false }),
+  productController.readyToShip
+);
 
-
+// AllProduct
+productRouter.get('/all_product/products', productController.getAllProductByCategory);
 
 module.exports = productRouter;
