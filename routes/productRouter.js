7496a68ebb5aa0f110ac2045f1de1e1134a2debd
Orating <p.thanawats@gmail.com>
2021-10-27 00:30:26 +0700

    solve productBug

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,8 +5,8 @@ const { upload } = require('../middleware/multer');
 
 const productRouter = express.Router();
 
-productRouter.get('/:productName', productController.getProductById)
-productRouter.get('/newAvailable/newAvailable', productController.getProductNewAvailable)
+productRouter.get('/byProductName/:productName', productController.getProductById)
+productRouter.get('arrival', productController.getProductNewArrival)
 productRouter.post('/checkFavorite', passport.authenticate('jwtAll', { session: false }), productController.checkFavorite)
 productRouter.post('/favorite', passport.authenticate('jwtAll', { session: false }), productController.createFavorite)
 productRouter.post('/cart', passport.authenticate('jwtAll', { session: false }), productController.createCartItem)
